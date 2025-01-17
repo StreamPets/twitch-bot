6fda15903f1d7cd3760f6e162aeb3b59f5659263
louisheal <louisheal@live.co.uk>
2025-01-17 13:55:06 +0000

    udpate endpoint urls

--- app/api.py
+++ app/api.py
@@ -7,22 +7,22 @@ class ApiService:
     self.api_url = api_url
 
   def announce_join(self, channel_name: str, user_id: str, username: str) -> None:
-    url = f"{self.api_url}/channels/{channel_name}/viewers"
+    url = f"{self.api_url}/channels/{channel_name}/users"
     requests.post(url, json={
       'user_id': user_id,
       'username': username,
     })
 
   def announce_part(self, channel_name: str, user_id: str) -> None:
-    url = f"{self.api_url}/channels/{channel_name}/viewers/{user_id}"
+    url = f"{self.api_url}/channels/{channel_name}/users/{user_id}"
     requests.delete(url)
 
   def announce_color(self, channel_name: str, user_id: str, color: str) -> None:
-    url = f"{self.api_url}/channels/{channel_name}/viewers/{user_id}"
+    url = f"{self.api_url}/channels/{channel_name}/users/{user_id}"
     requests.put(url, json={
       'item_name': color,
     })
 
   def announce_jump(self, channel_name: str, user_id: str) -> None:
-    url = f"{self.api_url}/channels/{channel_name}/viewers/{user_id}/JUMP"
+    url = f"{self.api_url}/channels/{channel_name}/users/{user_id}/JUMP"
     requests.post(url)
