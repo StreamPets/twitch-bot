57fa40b3e551f7aa5ddb8a6dde6e3242ff624bb7
louisheal <louisheal@live.co.uk>
2025-01-29 13:26:25 +0000

    replace channel name with channel id

--- app/api.py
+++ app/api.py
@@ -7,11 +7,11 @@ LOGGER: logging.Logger = logging.getLogger(__name__)
 
 async def announce_join(
     session: aiohttp.ClientSession,
-    channel_name: str,
+    channel_id: str,
     user_id: str,
     username: str,
 ) -> None:
-    url = f"/channels/{channel_name}/users"
+    url = f"/channels/{channel_id}/users"
     async with session.post(
         url,
         json={
@@ -25,10 +25,10 @@ async def announce_join(
 
 async def announce_part(
     session: aiohttp.ClientSession,
-    channel_name: str,
+    channel_id: str,
     user_id: str,
 ) -> None:
-    url = f"/channels/{channel_name}/users/{user_id}"
+    url = f"/channels/{channel_id}/users/{user_id}"
     async with session.delete(url) as response:
         if response.status != 204:
             LOGGER.error("received status code %d from announce part", response.status)
@@ -36,11 +36,11 @@ async def announce_part(
 
 async def announce_color(
     session: aiohttp.ClientSession,
-    channel_name: str,
+    channel_id: str,
     user_id: str,
     color: str,
 ) -> None:
-    url = f"/channels/{channel_name}/users/{user_id}"
+    url = f"/channels/{channel_id}/users/{user_id}"
     async with session.put(
         url,
         json={"item_name": color},
@@ -51,10 +51,10 @@ async def announce_color(
 
 async def announce_jump(
     session: aiohttp.ClientSession,
-    channel_name: str,
+    channel_id: str,
     user_id: str,
 ) -> None:
-    url = f"/channels/{channel_name}/users/{user_id}/JUMP"
+    url = f"/channels/{channel_id}/users/{user_id}/JUMP"
     async with session.post(url) as response:
         if response.status != 204:
             LOGGER.error("received status code %d from announce jump", response.status)
